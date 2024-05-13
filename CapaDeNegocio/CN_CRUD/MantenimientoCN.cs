e48d86f9641ae81b47f950ef089312e7e2ec224c
BYRON SALVADOR PICADO BERMÚDEZ <byron.picado23045970@estu.unan.edu.ni>
2024-05-13 16:47:30 -0600

    Cambios d

--- CapaDeNegocio/CN_CRUD/MantenimientoCN.cs
+++ CapaDeNegocio/CN_CRUD/MantenimientoCN.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CapaDeDatos.CRUD;
+using Entidades;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,27 @@ namespace CapaDeNegocio.CN_CRUD
 {
     internal class MantenimientoCN
     {
+        private MantenimientoCD mantenimientoCD = new MantenimientoCD();
+        public DataTable ObtenerMantenimiento()
+        {
+            DataTable tabla = new DataTable();
+            tabla = mantenimientoCD.ObtenerMantenimiento();
+            return tabla;
+        }
+        //pasar a capa de datos
+        public bool Insertar(Mantenimiento mantenimiento)
+        {
+            return mantenimientoCD.Insertar(mantenimiento);
+        }
+        public bool Editar(Mantenimiento mantenimiento)
+        {
+            return mantenimientoCD.Editar(mantenimiento);
+        }
+
+        public bool Eliminar(int mantenimientoId)
+        {
+            return mantenimientoCD.Eliminar(mantenimientoId);
+            return false;
+        }
     }
 }
